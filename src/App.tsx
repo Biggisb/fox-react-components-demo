a5d0c9a6b1ef725ee647c39a1e7535cf9d5b2af6
Burak.Topal <burak.topal@assecosol.com>
2020-12-04 14:37:11 +0100

    - removed IntrinsicElements - changed demo content to fully support reactifywc

--- src/App.tsx
+++ src/App.tsx
@@ -14,52 +14,66 @@ import reactifyWc from "reactify-wc";
 function App() {
 
 
-    const selected = (e: Event) => console.log("hello world", e);
+    function selected(e: Event) {
+        console.log("hello world", e);
+    }
 
     const FoxSelect = reactifyWc("fox-select") as any;
+    const FoxAppBar = reactifyWc("fox-app-bar") as any;
+    const FoxBackLayer = reactifyWc("fox-back-layer") as any;
+    const FoxFrontLayer = reactifyWc("fox-front-layer") as any;
+    const FoxContentLayer = reactifyWc("fox-content-layer") as any;
+    const FoxListItem = reactifyWc("fox-list-item") as any;
+    const FoxList = reactifyWc("fox-list") as any;
+    const FoxTextfield = reactifyWc("fox-textfield") as any;
+    const FoxCheckbox = reactifyWc("fox-checkbox") as any;
+    const FoxButton = reactifyWc("fox-button") as any;
 
     return (
 
         <div className="App">
 
-            <fox-app-bar></fox-app-bar>
+            <FoxAppBar></FoxAppBar>
             <div className="content-wrapper">
-                <fox-back-layer></fox-back-layer>
-                <fox-front-layer large>
-                    <fox-content-layer>
+                <FoxBackLayer></FoxBackLayer>
+                <FoxFrontLayer large>
+                    <FoxContentLayer>
                         <div slot="body" className="h-100 p-3">
                             <h4>FOX Select </h4>
 
                             <FoxSelect on-selected={selected}>
-                                <fox-list-item>1</fox-list-item>
-                                <fox-list-item>2</fox-list-item>
-                                <fox-list-item>3</fox-list-item>
-                                <fox-list-item>4</fox-list-item>
-                                <fox-list-item>5</fox-list-item>
+                                <FoxListItem>1</FoxListItem>
+                                <FoxListItem>2</FoxListItem>
+                                <FoxListItem>3</FoxListItem>
+                                <FoxListItem>4</FoxListItem>
+                                <FoxListItem>5</FoxListItem>
                             </FoxSelect>
 
                             <h4>FOX list </h4>
-                            <fox-list>
-                                <fox-list-item>1</fox-list-item>
-                                <fox-list-item>2</fox-list-item>
-                                <fox-list-item>3</fox-list-item>
-                                <fox-list-item>4</fox-list-item>
-                                <fox-list-item>5</fox-list-item>
-                            </fox-list>
+                            <FoxList>
+                                <FoxListItem>1</FoxListItem>
+                                <FoxListItem>2</FoxListItem>
+                                <FoxListItem>3</FoxListItem>
+                                <FoxListItem>4</FoxListItem>
+                                <FoxListItem>5</FoxListItem>
+                            </FoxList>
 
                             <h4>FOX textfield </h4>
-                            <fox-textfield label="My textfield"></fox-textfield>
+                            <div>
+                                <FoxTextfield label="My textfield"></FoxTextfield>
+                            </div>
+
 
                             <h4>FOX checkbox </h4>
-                            <fox-checkbox></fox-checkbox>
-                            <fox-checkbox checked></fox-checkbox>
+                            <FoxCheckbox></FoxCheckbox>
+                            <FoxCheckbox checked></FoxCheckbox>
 
                             <h4>FOX button </h4>
-                            <fox-button> Ich bin ein Button</fox-button>
-                            <fox-button outlined> Ich bin ein Button</fox-button>
+                            <FoxButton> Ich bin ein Button</FoxButton>
+                            <FoxButton outlined> Ich bin ein Button</FoxButton>
                         </div>
-                    </fox-content-layer>
-                </fox-front-layer>
+                    </FoxContentLayer>
+                </FoxFrontLayer>
             </div>
         </div>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -11,22 +11,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-declare global {
-    namespace JSX {
-        interface IntrinsicElements {
-            'fox-select':any;
-            'fox-list' : any;
-            'fox-list-item' : any;
-            'fox-textfield' : any;
-            'fox-checkbox':  any;
-            'fox-button':  any;
-            'fox-app-bar':  any;
-            'fox-back-layer':  any;
-            'fox-content-layer':  any;
-            'fox-front-layer':  any;
-        }
-    }
-}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
