6a3f979700c9dfd76c93a09da28f80613eb96a46
Burak.Topal <burak.topal@assecosol.com>
2021-06-14 11:21:54 +0200

    Test new fox-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,83 +1,78 @@
-import React from 'react';
-import './App.scss';
-import '@assecosolutions/fox-select';
-import '@assecosolutions/fox-list';
-import '@assecosolutions/fox-textfield';
-import '@assecosolutions/fox-checkbox';
-import '@assecosolutions/fox-button';
-import '@assecosolutions/fox-app-bar';
-import '@assecosolutions/fox-back-layer';
-import '@assecosolutions/fox-content-layer';
-import '@assecosolutions/fox-front-layer';
-import reactifyWc from "reactify-wc";
+import React from "react";
+import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
+import "./App.scss";
+import "@assecosolutions/fox-select";
+import "@assecosolutions/fox-list";
+import "@assecosolutions/fox-textfield";
+import "@assecosolutions/fox-app-bar";
+import "@assecosolutions/fox-back-layer";
+import "@assecosolutions/fox-content-layer";
+import "@assecosolutions/fox-front-layer";
+
+import "@assecosolutions/fox-card"; /*
+import "@assecosolutions/fox-checkbox";*/
+import "@assecosolutions/fox-button";
+import "@assecosolutions/fox-icon";
+import "@assecosolutions/fox-progress";
+
+export const step1 = () => (
+  <fox-content-layer>
+    <div slot="header" className="content-header">
+      <h2 className="p-3">Produktkonfigurator "Kugelschreiber"</h2>
+    </div>
+    <div slot="body" className="h-100 p-3 content-body">
+      <div className="row">
+        <fox-card class="col-6  ">
+          <div className="p-3">
+            <h3 className="text-client-base-color">Edel</h3>
+            <p>Kugelschreiber mit Aluminuiumspitze</p>
+            <div className="pt-2">
+              <a href="#">
+                Konfiguration <fox-icon>navigate_next</fox-icon>
+              </a>
+            </div>
+          </div>
+        </fox-card>
+        <fox-card class="col-6">22</fox-card>
+        <fox-card class="col-6">22</fox-card>
+        <fox-card class="col-6">22</fox-card>
+      </div>
+      <Router>
+        <div>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/step2">step2</Link>
+            <Link to="/step3">step3</Link>
+            <Link to="/step4">step4</Link>
+          </nav>
+        </div>
+      </Router>
+    </div>
+  </fox-content-layer>
+);
+export const step2 = () => <h1>Foo Page</h1>;
+export const step3 = () => <h1>Bar Page</h1>;
+export const step4 = () => <h1>Bar Page1</h1>;
 
 function App() {
-
-
-    function selected(e: Event) {
-        console.log("hello world", e);
-    }
-
-    const FoxSelect = reactifyWc("fox-select") as any;
-    const FoxAppBar = reactifyWc("fox-app-bar") as any;
-    const FoxBackLayer = reactifyWc("fox-back-layer") as any;
-    const FoxFrontLayer = reactifyWc("fox-front-layer") as any;
-    const FoxContentLayer = reactifyWc("fox-content-layer") as any;
-    const FoxListItem = reactifyWc("fox-list-item") as any;
-    const FoxList = reactifyWc("fox-list") as any;
-    const FoxTextfield = reactifyWc("fox-textfield") as any;
-    const FoxCheckbox = reactifyWc("fox-checkbox") as any;
-    const FoxButton = reactifyWc("fox-button") as any;
-
-    return (
-
-        <div className="App">
-
-            <FoxAppBar></FoxAppBar>
-            <div className="content-wrapper">
-                <FoxBackLayer></FoxBackLayer>
-                <FoxFrontLayer large>
-                    <FoxContentLayer>
-                        <div slot="body" className="h-100 p-3">
-                            <h4>FOX Select </h4>
-
-                            <FoxSelect on-selected={selected}>
-                                <FoxListItem>1</FoxListItem>
-                                <FoxListItem>2</FoxListItem>
-                                <FoxListItem>3</FoxListItem>
-                                <FoxListItem>4</FoxListItem>
-                                <FoxListItem>5</FoxListItem>
-                            </FoxSelect>
-
-                            <h4>FOX list </h4>
-                            <FoxList>
-                                <FoxListItem>1</FoxListItem>
-                                <FoxListItem>2</FoxListItem>
-                                <FoxListItem>3</FoxListItem>
-                                <FoxListItem>4</FoxListItem>
-                                <FoxListItem>5</FoxListItem>
-                            </FoxList>
-
-                            <h4>FOX textfield </h4>
-                            <div>
-                                <FoxTextfield label="My textfield"></FoxTextfield>
-                            </div>
-
-
-                            <h4>FOX checkbox </h4>
-                            <FoxCheckbox></FoxCheckbox>
-                            <FoxCheckbox checked></FoxCheckbox>
-
-                            <h4>FOX button </h4>
-                            <FoxButton> Ich bin ein Button</FoxButton>
-                            <FoxButton outlined> Ich bin ein Button</FoxButton>
-                        </div>
-                    </FoxContentLayer>
-                </FoxFrontLayer>
-            </div>
-        </div>
-    );
-
+  return (
+    <div className="App">
+      <fox-app-bar></fox-app-bar>
+      <div className="content-wrapper">
+        <fox-back-layer>AP Way?</fox-back-layer>
+        <fox-front-layer large>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={step1} />
+              <Route exact path="/step2" component={step2} />
+              <Route exact path="/step3" component={step3} />
+              <Route exact path="/step4" component={step4} />
+            </Switch>
+          </Router>
+        </fox-front-layer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,36 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import * as React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "@material-ui/icons";
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "fox-select": any;
+      "fox-list": any;
+      "fox-list-item": any;
+      "fox-textfield": any;
+      "fox-app-bar": any;
+      "fox-back-layer": any;
+      "fox-content-layer": any;
+      "fox-front-layer": any;
+      "fox-button": any;
+      "fox-icon": any;
+      "fox-checkbox": any;
+      "fox-card": any;
+      "fox-card-button": any;
+      "fox-progress": any;
+    }
+  }
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
